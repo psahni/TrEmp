c3ada2c5cbd90f7984020bc3ce02c8d2788010e6
Prashant Sahni <prashant.sahni5@gmail.com>
2014-02-05 20:16:45 +0530

    api/sessions controller create action

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,31 @@
 class Api::SessionsController < ApiController
+
+  respond_to :json
+
+  def create
+    #logger.debug "---------------------"
+    #logger.debug request.headers.inspect
+    user = User.where(email: params[:email]).first
+    if user && user.valid_password?(params[:password])
+      sign_in(:user, user)
+      render :json => {:success => true, :auth_token=> user.authentication_token, :email=> user.email}
+    else
+      invalid_login_attempt
+    end
+  end
+
+
+  protected
+
+  def invalid_login_attempt
+    render :json => {:success => false, :message=> "Error with your login or password" }, :status => 401
+  end
+
 end
+
+#curl -i -H "Accept: application/json" -H "Content-Type: application/json"-H "EMAIL:prashant@example.com" -d "email=prashant@gmail.com" http://localhost:5000/api/sessions.json
+
+
+# curl -i -H "EMAIL: prashant@gmail.com" -H "Accept: application/json" -d "email=prashant@gmail.com&password=sazsazwq" http://localhost:5000/api/sessions.json
+
+# The EMAIL header is available inside HTTP_EMAIL in the request object
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :ensure_authentication_token
+
+
+  private
+
+  def ensure_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
 end
